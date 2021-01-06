745e50b3d3b1a3471b53b741eecab17e1f84b5ff
hmlu36 <hmlu36@gmail.com>
2021-01-06 15:10:46 +0800

    'deploy'

--- src/actions/InvitationCard.js
+++ src/actions/InvitationCard.js
@@ -1,15 +1,8 @@
-const URL = require('../settings/url');
+const fs = require('fs');
 
 module.exports = function getInvitationCard(context) {
 
-    const cover = "1j2YbkMDpvyAIvsPm8I3hk-ASnmxN7hbo";
-    const content = "1WHESfjjey6k6OhiHll4Vnz86rTPbcqWE";
-    [cover, content].forEach(element => {
-
-        context.sendImage({
-            originalContentUrl: URL.INVITATION_CARD.replace("${id}", element) + "=w1124-h432",
-            previewImageUrl: URL.INVITATION_CARD.replace("${id}", element) + "=w337-h129",
-        });
-
+    ["invitation_cover.jpg", "invitation_content.jpg"].forEach(element => {
+        context.sendFile(fs.createReadStream('photos/' + element));
     })
 };
