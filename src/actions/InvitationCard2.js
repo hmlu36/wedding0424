33575d43cd51b2db7fcc541175c6b30c1a71f24d
hmlu36 <hmlu36@gmail.com>
2021-01-06 17:09:36 +0800

    'deploy'

--- src/actions/InvitationCard2.js
+++ src/actions/InvitationCard2.js
@@ -3,13 +3,13 @@ const URL = require('../settings/url');
 
 module.exports = async function getInvitationCard(context) {
 
-    const template = ["invitation_cover.jpg", "invitation_content.jpg"].map((photo) => {
+    const template = [{ lable: "封面", fileName: "invitation_cover.jpg" }, { label: "內頁", fileName: "invitation_content.jpg" }].map((item) => {
         return {
-            "imageUrl": URL.INVITATION_CARD.replace("${item}", photo),
+            "imageUrl": URL.INVITATION_CARD.replace("${item}", item.fileName),
             "action": {
                 "type": "uri",
-                "label": photo,
-                "uri": URL.INVITATION_CARD.replace("${item}", photo)
+                "label": item.label,
+                "uri": URL.INVITATION_CARD.replace("${item}", item.fileName)
             }
         }
     });
@@ -17,44 +17,3 @@ module.exports = async function getInvitationCard(context) {
     const altText = '電子喜帖';
     await context.sendImageCarouselTemplate(altText, template);
 }
-
-/*
-module.exports = async function getInvitationCard(context) {
-
-    const boxMessage = ["invitation_cover.jpg", "invitation_content.jpg"].map((photo) => {
-        return {
-            type: "bubble",
-            body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                    {
-                        type: "image",
-                        url: URL.INVITATION_CARD.replace("${item}", photo),
-                        size: "full",
-                        aspectMode: "cover",
-                        aspectRatio: "2:3",
-                        gravity: "top"
-                    }
-                ],
-                paddingAll: "0px"
-            }
-        };
-    });
-
-    //console.log(JSON.stringify(boxMessage));
-
-    sendCarouselMessage(context, boxMessage);
-}
-
-async function sendCarouselMessage(context, boxMessage) {
-    // 產生flex message格式
-    const flexMessage = {
-        type: "carousel",
-        contents: [...boxMessage]
-    };
-
-    //console.log(JSON.stringify(flexMessage));
-    await context.sendFlex('電子喜帖', flexMessage);
-}
-*/
