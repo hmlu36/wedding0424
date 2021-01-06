7acc6cf3cb3669d4e970228fca6be1a4936ed016
hmlu36 <hmlu36@gmail.com>
2021-01-06 16:39:25 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const InvitationCard2 = require('./actions/InvitationCard2');
 
 module.exports = function App(context) {
   return router([
-    text(/電子喜帖/i, InvitationCard),
-    text(/電子喜帖2/i, InvitationCard),
+    text(/電子喜帖.*$/i, InvitationCard),
+    text(/電子喜帖2.*$/i, InvitationCard),
     //text(/^婚禮地點.*$/i, AQI),
     //text(/^婚紗輪播.*$/i, Rain),
     //text(/^報名頁面.*$/i, Satellite),
