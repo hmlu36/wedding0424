69df9ff9cc18931ffd0e1c01878c3abecaddec16
hmlu36 <hmlu36@gmail.com>
2021-01-06 15:15:50 +0800

    'deploy'

--- src/actions/InvitationCard.js
+++ src/actions/InvitationCard.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 
 module.exports = function getInvitationCard(context) {
-
     ["invitation_cover.jpg", "invitation_content.jpg"].forEach(element => {
         context.sendFile(fs.createReadStream('photos/' + element));
-    })
+    });
 };
