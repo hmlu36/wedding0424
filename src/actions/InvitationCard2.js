3cfc579feeaa9c16273ab0d38a0b3e3e2df5118c
hmlu36 <hmlu36@gmail.com>
2021-01-06 16:54:08 +0800

    'deploy'

--- src/actions/InvitationCard2.js
+++ src/actions/InvitationCard2.js
@@ -9,6 +9,7 @@ module.exports = async function getInvitationCard(context) {
             "action": {
                 "type": "uri",
                 "label": photo,
+                "text": photo,
                 "uri": URL.INVITATION_CARD.replace("${item}", photo)
             }
         }
