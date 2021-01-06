88d0b422b5532390c2b381f10a2a73a65722d299
hmlu36 <hmlu36@gmail.com>
2021-01-06 12:01:06 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const { router, text } = require('bottender/router');
 const InvitationCard = require('./actions/InvitationCard');
 
 module.exports = function App(context) {
-    return router([
-      text(/電子喜帖.*$/i, InvitationCard),
-      //text(/^婚禮地點.*$/i, AQI),
-      //text(/^婚紗輪播.*$/i, Rain),
-      //text(/^報名頁面.*$/i, Satellite),
-      //text(/^留言板.*$/i, Temperature),
-      text('*', () => {
-        context.sendText(context.event.text);
-      }) // echo
-    ]);
-  };
+  return router([
+    text(/電子喜帖.*$/i, InvitationCard),
+    //text(/^婚禮地點.*$/i, AQI),
+    //text(/^婚紗輪播.*$/i, Rain),
+    //text(/^報名頁面.*$/i, Satellite),
+    //text(/^留言板.*$/i, Temperature),
+    text('*', (context) => {
+      context.sendText(context.event.text);
+    }) // echo
+  ]);
+};
