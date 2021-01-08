113c080362e6a7c45f4e2a383c66039f8c81943f
hmlu36 <hmlu36@gmail.com>
2021-01-08 10:09:09 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const DialogflowBot = require('./actions/DialogflowBot');
 
 module.exports = function App(context) {
   return router([
-    text(/(電子)喜帖/i, InvitationCard),
-    text(/(電子)喜帖2/i, InvitationCard2),
-    text(/(電子)喜帖3/i, InvitationCard3),
-    text(/(婚禮|婚宴)地點/i, Location),
+    text(/^([電子]?)喜帖$/i, InvitationCard),
+    text(/^([電子]?)喜帖2$/i, InvitationCard2),
+    text(/^([電子]?)喜帖3$/i, InvitationCard3),
+    text(/([(婚禮|婚宴)]?)地點/i, Location),
     //text(/^婚紗輪播.*$/i, Rain),
-    //text(/^報名頁面.*$/i, Satellite),
+    text(/^(報名([頁面]?)).*$/i, () => {
+      context.sendText("https://hmlu36.github.io/WeddingForm/");
+    }),
     //text(/^留言板.*$/i, Temperature),
     text(/^.*$/i, DialogflowBot)
     /*text(/^.*$/i, () => {
