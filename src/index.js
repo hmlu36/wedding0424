49270d9569ae32869711576c73606b56311aeaa5
hmlu36 <hmlu36@gmail.com>
2021-01-08 17:41:50 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { router, text } = require('bottender/router');
-const InvitationCard = require('./actions/InvitationCard');
+const InvitationCard = require('./actions/InvitationCard3');
 const InvitationCard2 = require('./actions/InvitationCard2');
-const InvitationCard3 = require('./actions/InvitationCard3');
+const InvitationCard3 = require('./actions/InvitationCard03');
 const Location = require('./actions/Location');
 const DialogflowBot = require('./actions/DialogflowBot');
 const URL = require('./settings/url');
@@ -9,14 +9,13 @@ const { CronJob } = require('cron');
 
 
 // 每天 7-23, 每30分鐘啟動crons, 避免heroku idle
-const scheduleOne = new CronJob(
+const cronJob = new CronJob(
   '0 0/30 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', // cron time
   () => {
     console.log("cron is running every 5 minutes prevent idle");
   }
 );
-
-
+cronJob.start();
 
 module.exports = function App(context) {
   return router([
