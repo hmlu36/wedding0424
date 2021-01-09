26974bb515f2dd513b406153ac8da868cc94a9b0
hmlu36 <hmlu36@gmail.com>
2021-01-09 22:44:53 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const DialogflowBot = require('./actions/DialogflowBot');
 const URL = require('./settings/url');
 const { CronJob } = require('cron');
 
-
+/*
 // 每天 7-23, 每30分鐘啟動crons, 避免heroku idle
 const cronJob = new CronJob(
-  '0 0/30 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', // cron time
+  '0 0/30 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', // cron time
   () => {
     console.log("cron is running every 5 minutes prevent idle");
   }
 );
 cronJob.start();
-
+*/
 
 module.exports = function App(context) {
   return router([
