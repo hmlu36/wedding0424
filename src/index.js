dde01fd883843cded85f39fbba387757b39f5dc6
hmlu36 <hmlu36@gmail.com>
2021-01-09 22:43:48 +0800

    'deploy'

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const { CronJob } = require('cron');
 
 // 每天 7-23, 每30分鐘啟動crons, 避免heroku idle
 const cronJob = new CronJob(
-  '0 0/30 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', // cron time
+  '0 0/30 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', // cron time
   () => {
     console.log("cron is running every 5 minutes prevent idle");
   }
