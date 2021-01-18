1d4e5965323f11b72fa5f55f96c9dc7a9c4a9114
hmlu36 <hmlu36@gmail.com>
2021-01-18 08:57:27 +0800

    'deploy'

--- src/settings/url.js
+++ src/settings/url.js
@@ -1,4 +1,4 @@
 module.exports = {
-    INVITATION_CARD: 'https://raw.githubusercontent.com/hmlu36/wedding424/main/photos/${item}',
-    GITHUB_REPOSITORY: 'https://hmlu36.github.io/WeddingForm/',
+    INVITATION_CARD: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/${item}',
+    WEDDINGFORM: 'https://hmlu36.github.io/WeddingForm/',
 };
