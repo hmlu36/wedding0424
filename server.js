2aac431da07c71bd69e6e5b4a0f2d425ca199d00
hmlu36 <hmlu36@gmail.com>
2021-01-27 16:54:10 +0800

    'deploy'

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
-const { createServer } = require('bottender/express');
+const bodyParser = require('body-parser');
+const express = require('express');
+const { bottender } = require('bottender');
 
-const bot = require('./bot'); // implement your bot logic
+const app = bottender({
+  dev: process.env.NODE_ENV !== 'production',
+});
 
-const server = createServer(bot);
+const port = Number(process.env.PORT) || 5000;
 
-server.listen(5000, () => {
-    console.log('server is running on 5000 port...');
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // If your express server is behind a proxy, you need to call `enable('trust proxy')`
+  // See: http://expressjs.com/en/guide/behind-proxies.html#express-behind-proxies
+  // server.enable('trust proxy');
+
+  const verify = (req, _, buf) => {
+    req.rawBody = buf.toString();
+  };
+  server.use(bodyParser.json({ verify }));
+  server.use(bodyParser.urlencoded({ extended: false, verify }));
+
+  server.get('/', (req, res) => {
+    res.json({ ok: true });
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
