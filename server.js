897d046ff90b5686cbca9f0d54bf5ff08df0cd12
hmlu36 <hmlu36@gmail.com>
2021-01-27 17:07:54 +0800

    'deploy'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { bottender } = require('bottender');
 
 const app = bottender({
-  dev: process.env.NODE_ENV !== 'production',
+    dev: process.env.NODE_ENV !== 'production',
 });
 
 const port = Number(process.env.PORT) || 5000;
@@ -11,28 +11,26 @@ const port = Number(process.env.PORT) || 5000;
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+    const server = express();
 
-  // If your express server is behind a proxy, you need to call `enable('trust proxy')`
-  // See: http://expressjs.com/en/guide/behind-proxies.html#express-behind-proxies
-  // server.enable('trust proxy');
 
-  const verify = (req, _, buf) => {
-    req.rawBody = buf.toString();
-  };
-  server.use(bodyParser.json({ verify }));
-  server.use(bodyParser.urlencoded({ extended: false, verify }));
+    const verify = (req, _, buf) => {
+        req.rawBody = buf.toString();
+    };
+    server.use(bodyParser.json({ verify }));
+    server.use(bodyParser.urlencoded({ extended: false, verify }));
 
-  server.get('/', (req, res) => {
-    res.json({ ok: true });
-  });
+    server.get('/api', (req, res) => {
+        res.json({ ok: true });
+    });
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
 
-  server.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(port, err => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 });
