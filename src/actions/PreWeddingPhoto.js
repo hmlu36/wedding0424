41b79dd3763af25bb335ef80d21345a0e67d589e
hmlu36 <hmlu36@gmail.com>
2021-02-25 14:40:54 +0800

    'deploy'

--- src/actions/PreWeddingPhoto.js
+++ src/actions/PreWeddingPhoto.js
@@ -3,13 +3,13 @@ const URL = require('../settings/url');
 
 module.exports = async function getPreWeddingPhoto(context) {
     // 產生flex message格式
-    const boxMessage = [...Array(5).keys()].map(index => {
+    const boxMessage = [...Array(4).keys()].map(index => {
         return {
             type: 'bubble',
             size: 'giga',
             hero: {
                 type: 'image',
-                aspectRatio: index == 1 ? '3:2' : '2:3',
+                aspectRatio: '2:3',
                 size: 'full',
                 aspectMode: 'cover',
                 url: `https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto${index + 1}.jpg`,
