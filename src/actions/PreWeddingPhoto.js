35526607f607569d1649d262bb2dd2fb9220ebfa
hmlu36 <hmlu36@gmail.com>
2021-02-25 11:36:25 +0800

    'deploy'

--- src/actions/PreWeddingPhoto.js
+++ src/actions/PreWeddingPhoto.js
@@ -2,32 +2,48 @@ const URL = require('../settings/url');
 
 
 module.exports = async function getPreWeddingPhoto(context) {
-    // 產生flex message格式
-    const flexMessage = {
-        type: 'template',
-        altText: '婚紗照',
-        template: {
-            type: 'image_carousel',
-            columns: [
-                {
-                    imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto1.jpg',
-                    action: {
-                        type: 'uri',
-                        label: '放大',
-                        uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto1.jpg'
-                    }
-                },
-                {
-                    imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto2.jpg',
-                    action: {
-                        type: 'uri',
-                        label: '放大',
-                        uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto2.jpg'
-                    }
-                }
-            ]
+    // 產生imageCarousel格式
+    const imageCarousel = [
+        {
+            imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto1.jpg',
+            action: {
+                type: 'uri',
+                label: '放大',
+                uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto1.jpg'
+            }
+        },
+        {
+            imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto2.jpg',
+            action: {
+                type: 'uri',
+                label: '放大',
+                uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto2.jpg'
+            }
+        },
+        {
+            imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto3.jpg',
+            action: {
+                type: 'uri',
+                label: '放大',
+                uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto3.jpg'
+            }
+        },
+        {
+            imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto4.jpg',
+            action: {
+                type: 'uri',
+                label: '放大',
+                uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto4.jpg'
+            }
+        },
+        {
+            imageUrl: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto5.jpg',
+            action: {
+                type: 'uri',
+                label: '放大',
+                uri: 'https://raw.githubusercontent.com/hmlu36/wedding0424/main/photos/PreWeddingPhoto5.jpg'
+            }
         }
-    };
-    await context.sendFlex('電子喜帖', flexMessage);
+    ];
+    await context.imageCarousel('婚紗照', imageCarousel);
 }
-
